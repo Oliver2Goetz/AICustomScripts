55519ac05c62026c5f6f3fc5e63a6d23f6e3824c
Oliver Goetz <oli.goetz.o.g@gmail.com>
2023-01-03 20:57:35 +0100

    Initial commit with example code

---
+++ AIFirstCustomScript/AIFirstCustomScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CATHODE;
+using CATHODE.Scripting;
+
+namespace AIFirstCustomScript {
+
+    class AIFirstCustomScript {
+
+        public static void Main() {
+            Commands commands = new Commands("COMMANDS.PAK");
+            Composite composite = commands.AddComposite("My coolScript", true);
+
+            FunctionEntity checkpoint = composite.AddFunction(FunctionType.Checkpoint);
+            checkpoint.AddParameter("is_first_checkpoint", new cBool(true));
+            checkpoint.AddParameter("section", new cString("Entry"));
+
+            FunctionEntity objective = composite.AddFunction(FunctionType.SetPrimaryObjective);
+            objective.AddParameter("title", new cString("Where Steve?"));
+            objective.AddParameter("additional_info", new cString("Go and find Steve!"));
+
+            checkpoint.AddParameterLink("finished_loading", objective, "trigger");
+
+            commands.Save();
+        }
+    }
+}
