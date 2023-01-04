1fb70996bb74fd3723032ff5ccba81c99453785e
Oliver Goetz <oli.goetz.o.g@gmail.com>
2023-01-04 01:38:20 +0100

    Update to CathodeLib 0.3.2, Added new example script, renamed project

--- AIFirstCustomScript/AICustomScripts.cs
+++ AIFirstCustomScript/AICustomScripts.cs
@@ -7,34 +7,37 @@ using System.Threading.Tasks;
 using CATHODE;
 using CATHODE.Scripting;
 
-namespace AIFirstCustomScript {
+namespace AICustomScripts {
 
-    class AIFirstCustomScript {
+    class AICustomScripts {
 
         private static string rootDirectory = "F:/SteamLibrary/steamapps/common/Alien Isolation/";
+
+        public static void Main() {
+
+        }
 
         /*
          * Using an existing COMMANDS.PAK and modify it (spawn player + add objective)
          */
-        public static void Main() {
+        public static void MainEditExistingPAK() {
             Commands commands = new Commands(rootDirectory + "DATA/ENV/PRODUCTION/ENG_ALIEN_NEST/WORLD/COMMANDS.PAK");
-
             Composite composite = commands.EntryPoints[0];
             composite.functions.Clear();
 
             FunctionEntity checkpoint = composite.AddFunction(FunctionType.Checkpoint);
-            FunctionEntity playerSpawn = composite.AddFunction("ARCHTYPES\\SCRIPT\\MISSION\\SPAWNPOSITIONSELECT");
+            FunctionEntity playerSpawn = composite.AddFunction(commands.GetComposite("ARCHETYPES\\SCRIPT\\MISSION\\SPAWNPOSITIONSELECT"));
 
             checkpoint.AddParameter("is_first_checkpoint", new cBool(true));
             checkpoint.AddParameter("section", new cString("Entry"));
 
-            checkpoint.AddParameterLink("finished_loading", playerSpawn, "Spawn player");
+            // Maybe add player position
+            cTransform cTransform = new cTransform();
+            cTransform.position = new CathodeLib.Vector3(0.1337f, 0.420f, 0.69f);
+            cTransform.rotation = new CathodeLib.Vector3(0, 0, 0);
+            playerSpawn.AddParameter("position", cTransform);
 
-            FunctionEntity objective = composite.AddFunction(FunctionType.SetPrimaryObjective);
-            objective.AddParameter("title", new cString("Where Stevies??"));
-            objective.AddParameter("additional_info", new cString("Go find him"));
-            checkpoint.AddParameterLink("finished_loading", objective, "trigger");
-
+            checkpoint.AddParameterLink("finished_loading", playerSpawn, "SpawnPlayer");
             commands.Save();
         }
 
